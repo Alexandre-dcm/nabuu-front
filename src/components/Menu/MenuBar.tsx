cbd305e24d7fbb446af28b4f47129fc8fc8bb710
Alex Dcm <a.dcm33@gmail.com>
2023-05-20 21:25:42 +0200

    Setting up home page

---
+++ src/components/Menu/MenuBar.tsx
@@ -0,0 +1,13 @@
+import "./MenuBar.css";
+
+const MenuBar = () => {
+    return <div className="menu-bar">
+        <div>
+            <li>Documentations</li>
+            <li>Articles</li>
+            <li>Log in</li>
+        </div>
+    </div>
+}
+
+export default MenuBar;
---
+++ src/pages/HomePage/index.tsx
@@ -0,0 +1,9 @@
+import HomeSection from "../../components/Home/HomeSection";
+
+const HomePage = () => {
+    return (
+        <HomeSection title='Nabuu !'></HomeSection>
+    );
+}
+
+export default HomePage
